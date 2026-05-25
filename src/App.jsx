e649c0845361d6e2e43030c10534329ee997476e
marcela-dorigao <marcela.dorigao@uel.br>
2026-05-25 20:13:46 -0300

    estrutura de páginas II

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import Home from './pages/Home';
-import AboutUs from './pages/AboutUs.jsx';
+{/*import AboutUs from './pages/AboutUs.jsx';}
 import Services from './pages/Services';
 import Gallery from './pages/Gallery.jsx';
-import Contact from './pages/Contact.jsx'
+import Contact from './pages/Contact.jsx'*}*/}
 
 function App() {
     return (
@@ -15,10 +15,10 @@ function App() {
                 <Route path="/" element={<Layout />}>
 
                     <Route index element={<Home />} />
-                    <Route path="aboutUs" element={<AboutUs/>} />
+                    {/*<Route path="aboutUs" element={<AboutUs/>} />
                     <Route path="services" element={<Services />} />
                     <Route path="gallery" element={<Gallery/>} />
-                    <Route path="contact" element={<Contact/>} />
+                    <Route path="contact" element={<Contact/>} />*/}
 
                 </Route>
             </Routes>
